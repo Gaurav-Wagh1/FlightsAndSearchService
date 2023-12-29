4fde0004c82499c8b6180608491720cd27984038
Gaurav-Wagh1 <gauravwagh0506@gmail.com>
2023-12-28 18:49:04 -1200

    Completed Basic Sequelize setup for crating a RDB

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const {PORT} = require('./config/serverconfig.js');
+const bodyParser = require('body-parser');
 
 const setupadnStartService = async() =>{
     // create a express object;
     const app = express();
+
+    app.use(bodyParser.json());
 
     app.listen(PORT, ()=>{
         console.log(`server listening at port :- ${PORT}`);
